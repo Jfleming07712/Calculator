e41d82fccb2d00501ddac3ef9f68e0d39fa6899c
Jfleming <Jfleming07712@gmail.com>
2019-06-12 13:56:24 -0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            Console.ReadLine();
+
+            var a = Console.ReadLine();
+
+           
+        }
+    }
+}
